94e41fb201ad2c136790fcf9f4728111b2961b1a
jagadishmurugaiyan382-cmyk <jagadishmurugaiyan382@gmail.com>
2026-03-13 19:10:38 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,124 @@
+from flask import Flask, render_template, request, redirect, session
+import db
+
+app = Flask(__name__)
+app.secret_key = "secret123"
+
+
+# ---------------- LOGIN ----------------
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        db.cursor.execute(
+            "SELECT * FROM users WHERE username=%s AND password=%s",
+            (username, password)
+        )
+
+        result = db.cursor.fetchone()
+
+        if result:
+            session["user"] = username
+            return redirect("/dashboard")
+        else:
+            return "Invalid login"
+
+    return render_template("login.html")
+
+
+# ---------------- REGISTER ----------------
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        db.cursor.execute(
+            "INSERT INTO users (username, password) VALUES (%s,%s)",
+            (username, password)
+        )
+
+        db.conn.commit()
+
+        return redirect("/")
+
+    return render_template("register.html")
+
+
+# ---------------- DASHBOARD ----------------
+
+@app.route("/dashboard")
+def dashboard():
+
+    if "user" in session:
+        return render_template("dashboard.html")
+
+    return redirect("/")
+@app.route("/users")
+def users():
+
+    db.cursor.execute("SELECT * FROM users")
+
+    data = db.cursor.fetchall()
+
+    return render_template("users.html", users=data)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+
+    db.cursor.execute(
+        "DELETE FROM users WHERE id=%s",
+        (id,)
+    )
+
+    db.conn.commit()
+
+    return redirect("/users")
+
+
+# ---------------- LOGOUT ----------------
+
+@app.route("/logout")
+def logout():
+
+    session.pop("user", None)
+    return redirect("/")
+
+
+@app.route("/edit/<int:id>", methods=["GET","POST"])
+def edit(id):
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        db.cursor.execute(
+            "UPDATE users SET username=%s, password=%s WHERE id=%s",
+            (username, password, id)
+        )
+
+        db.conn.commit()
+
+        return redirect("/users")
+
+    db.cursor.execute(
+        "SELECT * FROM users WHERE id=%s",
+        (id,)
+    )
+
+    user = db.cursor.fetchone()
+
+    return render_template("edit.html", user=user)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
